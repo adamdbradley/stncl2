6f491792c6dc658fbf2a70195b7e05681aa863b0
Adam Bradley <adambradley25@gmail.com>
2018-12-16 16:43:51 -0600

    wip

---
+++ www/build/wc-library.js
@@ -0,0 +1,72 @@
+
+export function h() {
+  console.log('h');
+}
+
+function setValue(memberName, instanceValues, newValue) {
+  const oldValue = instanceValues.get(memberName);
+  if (oldValue !== newValue) {
+    console.log('value changed from', oldValue, 'to', newValue);
+
+    instanceValues.set(memberName, newValue)
+    return true;
+  }
+  return false;
+}
+
+function render(instance) {
+  console.log('render')
+  instance.render();
+}
+
+export class IonCheckbox extends HTMLElement {
+
+  async connectedCallback() {
+    console.log('connected', this.tagName);
+    const elmData = {
+      instanceValues: new Map()
+    };
+    ref.set(this, elmData);
+
+    render(this);
+  }
+
+  render() {
+    return h('div', null);
+  }
+
+}
+
+const ref = new WeakMap();
+
+const style = 'div { color: blue }';
+
+
+function proxyMember(Cstr, memberName) {
+  console.log('proxyMember', memberName);
+
+  Object.defineProperty(Cstr.prototype, memberName, {
+    get() {
+      return ref.get(this).instanceValues.get(memberName);
+    },
+    set(newValue) {
+      const refItem = ref.get(this);
+      if (setValue(memberName, refItem.instanceValues, newValue)) {
+        if (refItem.instance) {
+          render(refItem.instance);
+        }
+      }
+    }
+  })
+}
+
+export function proxyComponent(Cstr, cmpMeta) {
+  console.log('proxyComponent', Cstr, cmpMeta);
+
+  cmpMeta[0].forEach(member => {
+    proxyMember(Cstr, member[0]);
+  });
+}
+
+proxyComponent(IonCheckbox, [['checked']])
+customElements.define('ion-checkbox', IonCheckbox)
